50b45cf1c5ae0d183894a19e2ed0a08190053785
Uli Koehler <ulikoehler@online.de>
2008-07-15 18:51:30 +0000

    GtkPwLister: Added code from SF pwgen project, finished coding GUI

--- GtkPwLister/main.cpp
+++ GtkPwLister/main.cpp
@@ -7,28 +7,40 @@
 #include <fstream>
 #include <boost/foreach.hpp>
 #include <boost/lexical_cast.hpp>
+#include "pwgen.h"
 using namespace std;
 using namespace boost;
 ///Macros and defines
 #define _(x) gettext(x)
-///Define appropriate field value of outputEntry
-#ifdef linux
-    #define OUTPUT_PRESET "/dev/shm/passwords.txt"
-#else
-    #define OUTPUT_PRESET "./passwords.txt"
-#endif
 
+///pwgen initialization
+int (*pw_number)(int max_num);
 
 ///Global variables
 GtkWidget *win;
 GtkWidget *table;
-GtkWidget *label; //Generic usage :-)
-GtkWidget *masterField;
+GtkWidget *label;
+GtkWidget *hbox;
 GtkWidget *lengthSpinButton;
-GtkWidget *outputHbox;
-GtkWidget *outputEntry;
-GtkWidget *outputTypeComboBox;
+//Capitalize radiobuttons
+GtkWidget *capNoneRb;
+GtkWidget *capOnRb;
+GtkWidget *capOffRb;
+//Numerals radiobuttons
+GtkWidget *numNoneRb;
+GtkWidget *numOnRb;
+GtkWidget *numOffRb;
+//Other option widget
+GtkWidget *symbolsCb;
+GtkWidget *secureCb;
+GtkWidget *vowelsOffCb;
+
 GtkWidget *okButton;
+
+int	pw_length = 8;
+int	num_pw = -1;
+int	pwgen_flags = 0;
+int	do_columns = 0;
 
 vector<string> pws;
 
@@ -38,41 +50,8 @@ void saveList();
 
 static void okButtonClicked(GtkWidget *wid, gpointer data)
 {
-    int length = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(lengthSpinButton));
-    static int decisionInt; ///Decides whether to use numbers oder (capital) letters
-    static unsigned char pwChar;
-    static string sbuf = "";
-    static string master(gtk_entry_get_text(GTK_ENTRY(masterField)));
-    RAND_seed(master.c_str(), master.length());
-    for (int i = 0; i < length;i++)
-        {
-            for(int j = 0;j < length; j++)
-                {
-                    RAND_pseudo_bytes(&pwChar, 1);
-                    RAND_pseudo_bytes(reinterpret_cast<unsigned char*>(&decisionInt), 1); //Fill decisionInt with random data
-                    switch(decisionInt % 3)
-                    {
-                        case 0: {pwChar = (pwChar % 11) + 48;break;} ///Number
-                        case 1: {pwChar = (pwChar % 26) + 65;break;} ///Uppercase letter
-                        case 2: {pwChar = (pwChar % 26) + 97;break;} ///Lowercase letter
-                        default: break;
-                    }
-                    sbuf.append(1, pwChar);
-                }
-            pws.push_back(sbuf);
-            sbuf.clear();
-        }
-    ///Now do what the user has chosen, e.g. showing or saving the list
-    switch(gtk_combo_box_get_active(GTK_COMBO_BOX(outputTypeComboBox)))
-        {
-            case 0: {showList();break;} ///Show
-            case 1: {saveList();break;} ///Save to text file
-            case 2: {break;} ///TODO: Save to PDF
-            default: break;
-        }
-    ///Cleanup
-    pws.clear();
-    RAND_cleanup();
+    pw_length = gtk_spin_button_get_value_as_int(GTK_SPIN_BUTTON(lengthSpinButton));
+
 }
 
 void showList()
@@ -100,15 +79,12 @@ void showList()
         }
     gtk_container_add(GTK_CONTAINER(listWindow), layout);
     gtk_widget_show_all(listWindow);
+    gtk_window_resize(GTK_WINDOW(win), 400, 400);
 }
 
-void saveList()
+void saveListPdf()
 {
-    fstream f(gtk_entry_get_text(GTK_ENTRY(outputEntry)), fstream::out);
-    BOOST_FOREACH(string s, pws)
-        {
-            f << s << "\n";
-        }
+
 }
 
 //static char** calculatePasswords(char* input, long length){return  NULL;}
@@ -121,39 +97,62 @@ int main (int argc, char *argv[])
 
   //Set some parameters
   gtk_window_set_title (GTK_WINDOW (win), _("GtkPwLister"));
+  gtk_container_set_border_width (GTK_CONTAINER (win), 3);
   gtk_window_set_position (GTK_WINDOW (win), GTK_WIN_POS_CENTER);
+  gtk_window_resize(GTK_WINDOW(win), 0, 0);
   gtk_window_set_resizable(GTK_WINDOW(win), false);
   gtk_widget_realize (win);
   gtk_table_set_row_spacings(GTK_TABLE(table), 3);
   gtk_table_set_col_spacings(GTK_TABLE(table), 3);
   g_signal_connect (win, "destroy", gtk_main_quit, NULL);
 
-  ///Init main widgets
-  label = gtk_label_new(_("Master password:"));
+  ///Init widgets
+  label = gtk_label_new(_("Length:"));
     gtk_table_attach_defaults(GTK_TABLE(table), label, 0, 1, 0, 1);
-  masterField  = gtk_entry_new();
-    gtk_table_attach_defaults(GTK_TABLE(table), masterField, 1, 2, 0, 1);
-  label = gtk_label_new(_("Length"));
-    gtk_table_attach_defaults(GTK_TABLE(table), label, 1, 2, 1, 2);
   lengthSpinButton = gtk_spin_button_new_with_range(1.0, 60.0, 1.0);
     gtk_spin_button_set_value(GTK_SPIN_BUTTON(lengthSpinButton), 6.0);
-    gtk_table_attach_defaults(GTK_TABLE(table), lengthSpinButton, 1, 2, 1, 2);
-  label = gtk_label_new(_("Output:"));
+    gtk_table_attach_defaults(GTK_TABLE(table), lengthSpinButton, 1, 2, 0, 1);
+  ////Capitals
+  label = gtk_label_new(_("Capitals:"));
+    gtk_table_attach_defaults(GTK_TABLE(table), label, 0, 1, 1, 2);
+  hbox = gtk_hbox_new(false, 3);
+    capNoneRb = gtk_radio_button_new_with_label(NULL, _("Random"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), capNoneRb);
+     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(capNoneRb), true);
+    capOnRb = gtk_radio_button_new_with_label(gtk_radio_button_group(GTK_RADIO_BUTTON(capNoneRb)), _("At least one"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), capOnRb);
+    capOffRb = gtk_radio_button_new_with_label(gtk_radio_button_group(GTK_RADIO_BUTTON(capOnRb)), _("None"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), capOffRb);
+  gtk_table_attach_defaults(GTK_TABLE(table), hbox, 1, 2, 1, 2);
+  ////Numerals
+  label = gtk_label_new(_("Numerals:"));
     gtk_table_attach_defaults(GTK_TABLE(table), label, 0, 1, 2, 3);
-  outputHbox = gtk_hbox_new(false, 3);
-    outputEntry = gtk_entry_new();
-        gtk_entry_set_text(GTK_ENTRY(outputEntry), OUTPUT_PRESET);
-        gtk_box_pack_start_defaults(GTK_BOX(outputHbox), outputEntry);
-    outputTypeComboBox = gtk_combo_box_new_text();
-        gtk_combo_box_append_text(GTK_COMBO_BOX(outputTypeComboBox), _("Show"));
-        gtk_combo_box_append_text(GTK_COMBO_BOX(outputTypeComboBox), _("Save"));
-        gtk_combo_box_append_text(GTK_COMBO_BOX(outputTypeComboBox), _("PDF"));
-        gtk_combo_box_set_active(GTK_COMBO_BOX(outputTypeComboBox), 0);
-        gtk_box_pack_start_defaults(GTK_BOX(outputHbox), outputTypeComboBox);
-    gtk_table_attach_defaults(GTK_TABLE(table), outputHbox, 1, 2, 2, 3);
+  hbox = gtk_hbox_new(false, 3);
+    numNoneRb = gtk_radio_button_new_with_label(NULL, _("Random"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), numNoneRb);
+     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(numNoneRb), true);
+    numOnRb = gtk_radio_button_new_with_label(gtk_radio_button_group(GTK_RADIO_BUTTON(numNoneRb)), _("At least one"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), numOnRb);
+    numOffRb = gtk_radio_button_new_with_label(gtk_radio_button_group(GTK_RADIO_BUTTON(numOnRb)), _("None"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), numOffRb);
+  gtk_table_attach_defaults(GTK_TABLE(table), hbox, 1, 2, 2, 3);
+  ////Other options
+  label = gtk_label_new(_("Other Options:"));
+    gtk_table_attach_defaults(GTK_TABLE(table), label, 0, 1, 3, 4);
+  hbox = gtk_hbox_new(false, 3);
+    symbolsCb = gtk_check_button_new_with_label(_("Symbols"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), symbolsCb);
+    secureCb = gtk_check_button_new_with_label(_("Secure"));
+     gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(secureCb), true);
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), secureCb);
+    vowelsOffCb = gtk_check_button_new_with_label(_("No vowels"));
+     gtk_box_pack_start_defaults(GTK_BOX(hbox), vowelsOffCb);
+  gtk_table_attach_defaults(GTK_TABLE(table), hbox, 1, 2, 3, 4);
+  ////OK Button
   okButton = gtk_button_new_with_label(_("OK"));
-    gtk_table_attach_defaults(GTK_TABLE(table), okButton, 0, 2, 3, 4);
+    gtk_table_attach_defaults(GTK_TABLE(table), okButton, 0, 2, 4, 5);
     g_signal_connect (okButton, "clicked", G_CALLBACK(okButtonClicked), NULL);
+  ///End init widgets
 
   /* Enter the main loop */
   gtk_container_add (GTK_CONTAINER (win), table);
