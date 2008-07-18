ea4c806d7eadd583320ca0c4d4fc5b3350f014cf
Uli Koehler <ulikoehler@online.de>
2008-07-18 15:14:23 +0000

    Averator: Everything works now,  this is release 0.1

--- Averator/main.cpp
+++ Averator/main.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <cmath>
 #include <gtk/gtk.h>
 #include <libintl.h>
 #include <vector>
@@ -7,6 +8,7 @@
 using namespace boost;
 using namespace std;
 #define _(x) gettext(x)
+#define root(x,n) pow(x, 1.0/n)
 
 GtkWidget *win;
 GtkWidget *vbox;
@@ -17,7 +19,7 @@ GtkWidget *addButton;
 GtkWidget *datasetLabel; //Shows how many datasets have been entered already
 GtkWidget *clearButton;
 GtkWidget *algorithmComboBox;
-GtkWidget *nEntry;
+GtkWidget *nSpinButton;
 GtkWidget *averageLabel;
 
 vector<long double> nums;
@@ -33,27 +35,48 @@ static void update(void)
             {
                 BOOST_FOREACH(long double e, nums)
                 {
-                    mean += e; //Directly using mean variable
+                    mean += e;
                 }
                 mean /= size;
                 break;
             }
         case 1: //Geometric
             {
+                mean = 1.0;
+                BOOST_FOREACH(long double e, nums)
+                {
+                    mean *= e;
+                }
+                mean =  root(mean, size);
                 break;
             }
         case 2: //Harmonic
             {
+                BOOST_FOREACH(long double e, nums)
+                {
+                    mean += 1.0/e;
+                }
+                mean = size/mean;
                 break;
             }
         case 3: //Power
             {
+                static int n = floor(gtk_spin_button_get_value(GTK_SPIN_BUTTON(nSpinButton)));
+                BOOST_FOREACH(long double e, nums)
+                {
+                    mean += pow(e, n);
+                }
+                mean /= size;
+                mean = root(mean, n);
                 break;
             }
         default: break;
     }
     gtk_label_set_text(GTK_LABEL(datasetLabel), lexical_cast<string>(size).c_str());
-    gtk_label_set_text(GTK_LABEL(averageLabel), lexical_cast<string>(mean).c_str());
+    if(size != 0)
+        {
+            gtk_label_set_text(GTK_LABEL(averageLabel), lexical_cast<string>(mean).c_str());
+        }
 }
 
 static void clearData(void)
@@ -63,27 +86,18 @@ static void clearData(void)
     gtk_label_set_text(GTK_LABEL(averageLabel), "");
 }
 
-static void processAlgorithmChange(void)
-{
-    switch(gtk_combo_box_get_active(GTK_COMBO_BOX(algorithmComboBox)))
-    {
-        case 3: //Power mean
-            {
-                gtk_entry_set_editable(GTK_ENTRY(nEntry), true);
-                break;
-            }
-        default: //Others
-            {
-                gtk_entry_set_editable(GTK_ENTRY(nEntry), false);
-                break;
-            }
-    }
-    update();
-}
-
 static void addNumber(void)
 {
-    nums.push_back(lexical_cast<long double>(gtk_entry_get_text(GTK_ENTRY(addEntry))));
+
+    string value(gtk_entry_get_text(GTK_ENTRY(addEntry)));
+    static string::size_type v = value.find(",", 0);
+    if(v != string::npos)
+        {
+            value[v] = '.';
+            gtk_entry_set_text(GTK_ENTRY(addEntry), value.c_str());
+        }
+    nums.push_back(lexical_cast<long double>(value));
+    gtk_widget_grab_focus(addEntry);
     update();
 }
 
@@ -108,6 +122,7 @@ int main (int argc, char *argv[])
   //Row to add numbers
   hbox = gtk_hbox_new(false, 5);
   addEntry = gtk_entry_new();
+   g_signal_connect(addEntry, "activate", addNumber, NULL);
    gtk_box_pack_start_defaults(GTK_BOX(hbox), addEntry);
   addButton = gtk_button_new_with_label(_("Add"));
    g_signal_connect (addButton, "clicked", addNumber, NULL);
@@ -130,15 +145,14 @@ int main (int argc, char *argv[])
    gtk_combo_box_append_text(GTK_COMBO_BOX(algorithmComboBox), _("Harmonic mean"));
    gtk_combo_box_append_text(GTK_COMBO_BOX(algorithmComboBox), _("Power mean"));
    gtk_combo_box_set_active(GTK_COMBO_BOX(algorithmComboBox), 0);
-   g_signal_connect (algorithmComboBox, "changed", processAlgorithmChange, NULL);
+   g_signal_connect (algorithmComboBox, "changed", update, NULL);
    gtk_box_pack_start_defaults(GTK_BOX(hbox), algorithmComboBox);
   label = gtk_label_new(_("n:"));
    gtk_box_pack_start_defaults(GTK_BOX(hbox), label);
-  nEntry = gtk_entry_new();
-   gtk_entry_set_text(GTK_ENTRY(nEntry), "2");
-   gtk_entry_set_width_chars(GTK_ENTRY(nEntry), 3);
-   gtk_entry_set_editable(GTK_ENTRY(nEntry), false);
-   gtk_box_pack_start_defaults(GTK_BOX(hbox), nEntry);
+  nSpinButton = gtk_spin_button_new_with_range(2, 9999, 1.0);
+   gtk_spin_button_set_value(GTK_SPIN_BUTTON(nSpinButton), 2);
+   g_signal_connect (nSpinButton, "change-value", update, NULL);
+   gtk_box_pack_start_defaults(GTK_BOX(hbox), nSpinButton);
   gtk_box_pack_start_defaults(GTK_BOX(vbox), hbox);
   //Result row
   hbox = gtk_hbox_new(false, 5);
