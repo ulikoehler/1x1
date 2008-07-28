e7819b982f8efe9d0565ff20878046852c6ff184
Uli Koehler <ulikoehler@online.de>
2008-07-28 12:47:03 +0000

    JTimeBomb: Added project; finished building GUI

---
+++ JTimeBomb/src/jtimebomb/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jtimebomb;
+
+/**
+ *
+ * @author uli
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JTimeBombFrame mainFrame = new JTimeBombFrame();
+        mainFrame.show();
+    }
+
+}
