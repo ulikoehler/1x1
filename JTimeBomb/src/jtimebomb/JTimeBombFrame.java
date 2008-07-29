2092da608b861efa2e4f55dc1a6300ab29c076f1
Uli Koehler <ulikoehler@online.de>
2008-07-29 13:03:42 +0000

    JTimeBomb: Added algorithms and simplified GUI, everything works except the timer calculation and the speed of timer task execution

--- JTimeBomb/src/jtimebomb/JTimeBombFrame.java
+++ JTimeBomb/src/jtimebomb/JTimeBombFrame.java
@@ -5,6 +5,10 @@ package jtimebomb;
  *
  * Created on 28. Juli 2008, 12:22
  */
+import java.awt.Color;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.SpinnerNumberModel;
 
 
 
@@ -13,6 +17,14 @@ package jtimebomb;
  * @author  uli
  */
 public class JTimeBombFrame extends javax.swing.JFrame {
+    
+    //Custom components
+    int totalSecondsLeft; //In seconds, overall
+    int hoursLeft;
+    int minutesLeft; //Not overall
+    int secondsLeft; //Not overall
+    Timer timer;
+    final int delay = 1000;
     
     /** Creates new form JTImeBombFrame */
     public JTimeBombFrame() {
@@ -29,14 +41,13 @@ public class JTimeBombFrame extends javax.swing.JFrame {
 
         timeLeftLabel = new javax.swing.JLabel();
         statusLabel = new javax.swing.JLabel();
-        passwordField = new javax.swing.JTextField();
-        jSpinner1 = new javax.swing.JSpinner();
-        jSpinner2 = new javax.swing.JSpinner();
-        jSpinner3 = new javax.swing.JSpinner();
+        hourSpinner = new javax.swing.JSpinner();
+        minuteSpinner = new javax.swing.JSpinner();
+        secondSpinner = new javax.swing.JSpinner();
         hourLabel = new javax.swing.JLabel();
-        hourLabel1 = new javax.swing.JLabel();
-        hourLabel2 = new javax.swing.JLabel();
-        jProgressBar1 = new javax.swing.JProgressBar();
+        minutesLabel = new javax.swing.JLabel();
+        secondsLabel = new javax.swing.JLabel();
+        statusBar = new javax.swing.JProgressBar();
         activateToggleButton = new javax.swing.JToggleButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -50,7 +61,7 @@ public class JTimeBombFrame extends javax.swing.JFrame {
         timeLeftLabel.setBackground(new java.awt.Color(0, 0, 0));
         timeLeftLabel.setFont(new java.awt.Font("Gunplay", 0, 36));
         timeLeftLabel.setForeground(new java.awt.Color(255, 0, 51));
-        timeLeftLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        timeLeftLabel.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         timeLeftLabel.setText("00:00:00");
 
         statusLabel.setFont(new java.awt.Font("Gunplay", 0, 18));
@@ -58,13 +69,24 @@ public class JTimeBombFrame extends javax.swing.JFrame {
         statusLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         statusLabel.setText("Deactivated");
 
+        hourSpinner.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+
+        minuteSpinner.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+
+        secondSpinner.setModel(new SpinnerNumberModel(0, 0, 59, 1));
+
         hourLabel.setText("Hours:");
 
-        hourLabel1.setText("Minutes:");
+        minutesLabel.setText("Minutes:");
 
-        hourLabel2.setText("Seconds:");
+        secondsLabel.setText("Seconds:");
 
         activateToggleButton.setText("Activate");
+        activateToggleButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                activateToggleButtonMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -73,29 +95,26 @@ public class JTimeBombFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(activateToggleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(timeLeftLabel)
-                            .addGroup(layout.createSequentialGroup()
-                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                                    .addComponent(passwordField, javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jProgressBar1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
+                            .addComponent(statusBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                             .addComponent(statusLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 136, Short.MAX_VALUE)
                             .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
                                 .addGap(9, 9, 9)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(hourLabel2)
-                                    .addComponent(hourLabel1)
+                                    .addComponent(secondsLabel)
+                                    .addComponent(minutesLabel)
                                     .addComponent(hourLabel))
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                    .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                    .addComponent(jSpinner3, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE)))))
+                                    .addComponent(minuteSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(hourSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(secondSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 11, Short.MAX_VALUE))))
+                    .addComponent(activateToggleButton, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -107,28 +126,65 @@ public class JTimeBombFrame extends javax.swing.JFrame {
                         .addComponent(statusLabel)
                         .addGap(32, 32, 32)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(hourLabel1)
-                            .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(minutesLabel)
+                            .addComponent(minuteSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                            .addComponent(hourLabel2)
-                            .addComponent(jSpinner3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                            .addComponent(secondsLabel)
+                            .addComponent(secondSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(timeLeftLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(hourSpinner, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(hourLabel))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jProgressBar1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(passwordField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(statusBar, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(activateToggleButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(22, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void activateToggleButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_activateToggleButtonMouseClicked
+        // TODO add your handling code here:
+        //Check whether to activate or to deactivate the bomb
+        if(activateToggleButton.isSelected()) //Try to deactivate the bomb
+            {
+                //Get data
+                SpinnerNumberModel model = (SpinnerNumberModel) hourSpinner.getModel();
+                    hoursLeft = model.getNumber().intValue();
+                model = (SpinnerNumberModel) minuteSpinner.getModel();
+                    minutesLeft = model.getNumber().intValue();
+                model = (SpinnerNumberModel) secondSpinner.getModel();
+                    secondsLeft = model.getNumber().intValue();
+                totalSecondsLeft = secondsLeft + minutesLeft * 60 + hoursLeft * 3600;
+                //Initialize status bar
+                 statusBar.setMaximum(totalSecondsLeft);
+                 statusBar.setValue(totalSecondsLeft);
+                //Initialize main (big) timer Label
+                 String timerString = Integer.toString(hoursLeft) + ":" + Integer.toString(minutesLeft) + ":" + Integer.toString(secondsLeft);
+                 timeLeftLabel.setText(timerString);
+                //Set state label value and color
+                statusLabel.setText("Activated");
+                statusLabel.setForeground(new Color(255,0,0));
+                //Activate timer
+                timer = new Timer();
+                timer.schedule(new TimerTask() {
+                                                    public void run()
+                                                        {
+                                                            timerTick();
+                                                        }
+                                                }, 1000, totalSecondsLeft);
+            }
+        else //Try to activate the bomb
+            {
+                timer.cancel();
+                statusLabel.setText("Defused");
+                statusLabel.setForeground(new Color(0,255,0));
+            }
+    }//GEN-LAST:event_activateToggleButtonMouseClicked
     
     /**
      * @param args the command line arguments
@@ -144,15 +200,33 @@ public class JTimeBombFrame extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JToggleButton activateToggleButton;
     private javax.swing.JLabel hourLabel;
-    private javax.swing.JLabel hourLabel1;
-    private javax.swing.JLabel hourLabel2;
-    private javax.swing.JProgressBar jProgressBar1;
-    private javax.swing.JSpinner jSpinner1;
-    private javax.swing.JSpinner jSpinner2;
-    private javax.swing.JSpinner jSpinner3;
-    private javax.swing.JTextField passwordField;
+    private javax.swing.JSpinner hourSpinner;
+    private javax.swing.JSpinner minuteSpinner;
+    private javax.swing.JLabel minutesLabel;
+    private javax.swing.JSpinner secondSpinner;
+    private javax.swing.JLabel secondsLabel;
+    private javax.swing.JProgressBar statusBar;
     private javax.swing.JLabel statusLabel;
     private javax.swing.JLabel timeLeftLabel;
     // End of variables declaration//GEN-END:variables
+
+    public void timerTick()
+    {
+        totalSecondsLeft--;
+        if(totalSecondsLeft==0) //If bomb has detonated
+            {
+                timer.cancel();
+                statusLabel.setText("Detonated");
+                return;
+            }
+        //Calculate time left
+        secondsLeft = totalSecondsLeft%60;
+        minutesLeft = (totalSecondsLeft%3600)-secondsLeft;
+        hoursLeft = ((totalSecondsLeft-secondsLeft)-minutesLeft)/3600;
+        statusBar.setValue(totalSecondsLeft);
+        //Initialize main (big) timer Label
+        String timerString = Integer.toString(hoursLeft) + ":" + Integer.toString(minutesLeft) + ":" + Integer.toString(secondsLeft);
+        timeLeftLabel.setText(timerString);
+    }
     
 }
