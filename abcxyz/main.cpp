654f26ef7f86295f72e7ed83f9a49266847482ba
Uli Koehler <ulikoehler@online.de>
2008-10-16 17:04:49 +0000

    abcxyz: Added statistics code and abc % N == 0 filtering (boost thread now needed

--- abcxyz/main.cpp
+++ abcxyz/main.cpp
@@ -6,24 +6,30 @@
 #include <cmath>
 #include <stdio.h>
 #include <iostream>
-#include <MersenneTwister.h>
 
 #include <cln/integer.h>
 #include <cln/integer_io.h>
+#include <cln/ring.h>
+#include <cln/modinteger.h>
+
+#include <boost/thread/thread.hpp>
 
 using namespace std;
 using namespace cln;
+using namespace boost;
 
 //Operation defins
 #define root(n,x) pow(x, 1.0/n)
-#define inc(x) x = x+1;
+#define inc(x) x=x+1
 
-#define ATRIES
+#define ATRIES 100
 #define BTRIES 100
 #define ABCLIMIT 150
 #define XYZLIMIT 20
 #define ABCSV 2
 #define XYZSV 2
+#define MODN 3
+#define DISPLAYINTERVAL 5
 
 #define cdc cl_decimal_string
 
@@ -31,41 +37,12 @@ using namespace cln;
 //Switches
 //#define DEBUG
 #define OPFIRST
+#define DISPLAYSTATS
 
 //Statements to switch abc and xyz at compile time e.g. for behaviour: operators change first
 
 typedef unsigned long long ull;
 typedef unsigned long ul;
-
-
-//ull rpw(ull b, ull e) //Recursive power, slow but using 64-bit ull numbers
-//{
-//    ull pow = 1;
-//    #pragma omp parallel for reduction(*:pow)
-//    for(ull i = 0; i < e; i++)
-//    {
-//        pow *= b;
-//    }
-//    return pow;
-//}
-//
-//ull rpwa(ull b, ull e, ull b2, ull e2) //Recursive power, slow but using 64-bit ull numbers
-//{
-//    ull pow = 1;
-//    #pragma omp parallel for reduction(*:pow)
-//    for(ull i = 0; i < e; i++)
-//    {
-//        pow *= b;
-//    }
-//    ull pow2 = 1;
-//    for(ull i = 0; i < e; i++)
-//    {
-//        pow *= b;
-//    }
-//    return pow;
-//}
-
-
 
 /** Testing main function**/
 //int main()
@@ -76,17 +53,29 @@ typedef unsigned long ul;
 /*
  *
  */
+ull tried = 0;
+
+
 int main(int argc, char** argv)
 {
-    FILE *f = fopen("/ram/formulasd.txt", "w");
-
-    cl_I a = ABCSV;
-    cl_I b = ABCSV;
-    cl_I c = ABCSV;
+    //Variable declarations
+    cl_I a;
+    cl_I b;
+    cl_I c;
 
     cl_I x = XYZSV;
     cl_I y = XYZSV;
     cl_I z = XYZSV;
+
+    //Used for mod operations
+    cl_modint_ring modProvider = find_modint_ring(MODN);
+
+    int display();
+
+    #ifdef DISPLAYSTATS
+        //Start stats thread
+        thread statsThread(&display);
+    #endif
 
     //Main loop
     for (; z <= XYZLIMIT; inc(x))
@@ -102,23 +91,30 @@ int main(int argc, char** argv)
                         cout << "b: " << b << "    y: " << y << "      ";
                         cout << "c: " << c << "    z: " << z << endl;
                     #endif
+
+                    if((a == c && x == z) || (b == c && y == z)) {continue;}
+
+                    if(modProvider->canonhom(a) != 0 || modProvider->canonhom(b) != 0 || modProvider->canonhom(b) != 0) {continue;}
+
                     if(    a == x || a == b || a == y || a == c || a == z
                         || x == b || x == y || x == c || x == z
                         || b == y || b == c || b == z
                         || y == c || y == z
                         || c == z) {continue;}
 
-                    if((a == c && x == z) || (b == c && y == z)) {continue;}
 
                     cl_I leftSide = expt_pos(a, x) + expt_pos(b, y);
                     cl_I rightSide = expt_pos(c, z);
+
 
                     if (leftSide == rightSide)
                     {
                         //Check if !exp == a^x + b^x = b^x
                         printf("Found solution: %s^%s + %s^%s = %s^%s\n", cdc(a), cdc(x), cdc(b), cdc(y), cdc(c), cdc(z));
-                        fprintf(f, "%s^%s + %s^%s = %s^%s\n", cdc(a), cdc(x), cdc(b), cdc(y), cdc(c), cdc(z));
                     }
+                    //#ifdef DISPLAYSTATS
+                        tried++;
+                    //#endif
                 }
             }
         }
@@ -134,7 +130,18 @@ int main(int argc, char** argv)
             }
         }
     }
-    fclose(f);
+
+    endwin();
     return (EXIT_SUCCESS);
 }
 
+int display()
+{
+    while(true)
+    {
+        deleteln();
+        sleep(DISPLAYINTERVAL);
+        cerr << "Expressions tried: " << tried;
+    }
+    return 0;
+}
