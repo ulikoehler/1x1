8cfd88feca132d652d89faef6c5fd352ae715221
Uli Koehler <ulikoehler@online.de>
2009-01-07 19:31:04 +0000

    JRocketStartSimulator: Added project. Purpose: Simulate a simple rocket start + a physics test project

--- JRocketStartSimulator/src/jrocketstartsimulator/JRocketStartSimulatorFrame.java
+++ JRocketStartSimulator/src/jrocketstartsimulator/JRocketStartSimulatorFrame.java
@@ -11,7 +11,12 @@
 
 package jrocketstartsimulator;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -39,11 +44,17 @@ public class JRocketStartSimulatorFrame extends javax.swing.JFrame {
         fuelVelocityLabel = new javax.swing.JLabel();
         gravityLabel = new javax.swing.JLabel();
         okButton = new javax.swing.JButton();
-        gravityField = new javax.swing.JFormattedTextField();
-        fuelVelocityField = new javax.swing.JFormattedTextField();
-        fuelEmissionField = new javax.swing.JFormattedTextField();
-        fuelWeightField = new javax.swing.JFormattedTextField();
-        rocketWeightField = new javax.swing.JFormattedTextField();
+        resolutionLabel = new javax.swing.JLabel();
+        resolutionField = new javax.swing.JTextField();
+        gravityField = new javax.swing.JTextField();
+        fuelVelocityField = new javax.swing.JTextField();
+        fuelEmissionField = new javax.swing.JTextField();
+        fuelWeightField = new javax.swing.JTextField();
+        rocketWeightField = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        outputTextArea = new javax.swing.JTextArea();
+        precisionLabel = new javax.swing.JLabel();
+        precisionField = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle( i18n.getString("JRocketStartSimulatorFrame.title")); // NOI18N
@@ -59,16 +70,33 @@ public class JRocketStartSimulatorFrame extends javax.swing.JFrame {
         gravityLabel.setText( i18n.getString("JRocketStartSimulatorFrame.gravityLabel.text")); // NOI18N
 
         okButton.setText( i18n.getString("JRocketStartSimulatorFrame.okButton.text")); // NOI18N
+        okButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                okButtonMouseClicked(evt);
+            }
+        });
 
-        gravityField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("########0.##################### m/s²"))));
+        resolutionLabel.setText( i18n.getString("JRocketStartSimulatorFrame.resolutionLabel.text")); // NOI18N
 
-        fuelVelocityField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("#,##0.### m/s"))));
+        resolutionField.setText( i18n.getString("JRocketStartSimulatorFrame.resolutionField.text")); // NOI18N
 
-        fuelEmissionField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("########0.##################### kg/s"))));
+        gravityField.setText( i18n.getString("JRocketStartSimulatorFrame.gravityField.text")); // NOI18N
 
-        fuelWeightField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat("########0.##################### kg"))));
+        fuelVelocityField.setText( i18n.getString("JRocketStartSimulatorFrame.fuelVelocityField.text")); // NOI18N
 
-        rocketWeightField.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.NumberFormatter(new java.text.DecimalFormat(""))));
+        fuelEmissionField.setText( i18n.getString("JRocketStartSimulatorFrame.fuelEmissionField.text")); // NOI18N
+
+        fuelWeightField.setText( i18n.getString("JRocketStartSimulatorFrame.fuelWeightField.text")); // NOI18N
+
+        rocketWeightField.setText( i18n.getString("JRocketStartSimulatorFrame.rocketWeightField.text")); // NOI18N
+
+        outputTextArea.setColumns(20);
+        outputTextArea.setRows(5);
+        jScrollPane1.setViewportView(outputTextArea);
+
+        precisionLabel.setText( i18n.getString("JRocketStartSimulatorFrame.precisionLabel.text")); // NOI18N
+
+        precisionField.setText( i18n.getString("JRocketStartSimulatorFrame.precisionField.text")); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -77,27 +105,26 @@ public class JRocketStartSimulatorFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(rocketWeightLabel)
-                        .addGap(64, 64, 64)
-                        .addComponent(rocketWeightField, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(fuelWeightLabel)
-                        .addGap(53, 53, 53)
-                        .addComponent(fuelWeightField, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(gravityLabel)
-                        .addGap(88, 88, 88)
-                        .addComponent(gravityField, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE))
-                    .addComponent(okButton, javax.swing.GroupLayout.DEFAULT_SIZE, 379, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(gravityLabel)
+                            .addComponent(fuelWeightLabel)
+                            .addComponent(rocketWeightLabel)
+                            .addComponent(fuelEmissionLabel)
+                            .addComponent(resolutionLabel)
                             .addComponent(fuelVelocityLabel)
-                            .addComponent(fuelEmissionLabel))
+                            .addComponent(precisionLabel))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(fuelEmissionField, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE)
-                            .addComponent(fuelVelocityField, javax.swing.GroupLayout.DEFAULT_SIZE, 219, Short.MAX_VALUE))))
+                            .addComponent(fuelVelocityField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                            .addComponent(fuelWeightField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                            .addComponent(rocketWeightField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                            .addComponent(fuelEmissionField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                            .addComponent(gravityField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                            .addComponent(resolutionField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)
+                            .addComponent(precisionField, javax.swing.GroupLayout.DEFAULT_SIZE, 240, Short.MAX_VALUE)))
+                    .addComponent(okButton, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -115,21 +142,106 @@ public class JRocketStartSimulatorFrame extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(fuelEmissionLabel)
                     .addComponent(fuelEmissionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(7, 7, 7)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(fuelVelocityLabel)
-                    .addComponent(fuelVelocityField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                    .addComponent(fuelVelocityField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(fuelVelocityLabel))
+                .addGap(7, 7, 7)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(gravityLabel)
                     .addComponent(gravityField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(resolutionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(resolutionLabel))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(precisionLabel)
+                    .addComponent(precisionField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(okButton)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 209, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void okButtonMouseClicked(java.awt.event.MouseEvent evt)//GEN-FIRST:event_okButtonMouseClicked
+    {//GEN-HEADEREND:event_okButtonMouseClicked
+        FileWriter fw = null;
+        try
+        {
+            double fuelWeight =
+                    new Double(fuelWeightField.getText());
+            double rocketWeight = new Double(rocketWeightField.getText());
+            double mass =
+                     rocketWeight + fuelWeight;
+            double fuelEm = new Double(fuelEmissionField.getText());
+            double fuelVel = new Double(fuelVelocityField.getText());
+            double gravity = new Double(gravityField.getText());
+            double res = new Double(resolutionField.getText());
+            double nForce = fuelEm * fuelVel;
+            double newtonWeight = mass * gravity;
+            double height = 0.0;
+            double time = 0.0; //Elapsed time
+            double velocity = 0.0; //Entire rocket velocity
+            double acceleration;
+
+            ///Build the format string
+            String formatstring = "%." + precisionField.getText() + "f";
+
+            fw = new FileWriter("rocket.csv");
+
+            outputTextArea.append("t , m , a , v , h\n");
+            
+            for(int i = 0; i < 10000;i++)
+            {
+                time += res;
+                acceleration = (nForce - newtonWeight) / mass;
+                if(fuelWeight > 0) {fuelWeight -= fuelEm;}
+                else {fuelWeight = 0;}
+                //Recalculate scalars
+                if(fuelWeight > 0) {velocity += acceleration * res;}
+                if(velocity < 0) {break;}
+                height += velocity;
+                mass = rocketWeight + fuelWeight;
+                newtonWeight = mass * gravity;
+                //Print out the results
+                StringBuilder sb = new StringBuilder();
+                sb.append(String.format(Locale.US, formatstring, time));
+                sb.append(" , ");
+                sb.append(String.format(Locale.US, formatstring, mass));
+                sb.append(" , ");
+                sb.append(String.format(Locale.US, formatstring, acceleration));
+                sb.append(" , ");
+                sb.append(String.format(Locale.US, formatstring, velocity));
+                sb.append(" , ");
+                sb.append(String.format(Locale.US, formatstring, height));
+                sb.append("\n");
+                fw.write(sb.toString());
+                outputTextArea.append(sb.toString());
+            }
+            fw.close();
+        }
+        catch (IOException ex)
+        {
+            Logger.getLogger(JRocketStartSimulatorFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        finally
+        {
+            try
+            {
+                fw.close();
+            }
+            catch(NullPointerException ex){}
+            catch (IOException ex)
+            {
+                Logger.getLogger(JRocketStartSimulatorFrame.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_okButtonMouseClicked
 
     /**
     * @param args the command line arguments
@@ -145,16 +257,22 @@ public class JRocketStartSimulatorFrame extends javax.swing.JFrame {
 
     private ResourceBundle i18n = ResourceBundle.getBundle("jrocketstartsimulator/Bundle"); //NOI18N
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JFormattedTextField fuelEmissionField;
+    private javax.swing.JTextField fuelEmissionField;
     private javax.swing.JLabel fuelEmissionLabel;
-    private javax.swing.JFormattedTextField fuelVelocityField;
+    private javax.swing.JTextField fuelVelocityField;
     private javax.swing.JLabel fuelVelocityLabel;
-    private javax.swing.JFormattedTextField fuelWeightField;
+    private javax.swing.JTextField fuelWeightField;
     private javax.swing.JLabel fuelWeightLabel;
-    private javax.swing.JFormattedTextField gravityField;
+    private javax.swing.JTextField gravityField;
     private javax.swing.JLabel gravityLabel;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton okButton;
-    private javax.swing.JFormattedTextField rocketWeightField;
+    private javax.swing.JTextArea outputTextArea;
+    private javax.swing.JTextField precisionField;
+    private javax.swing.JLabel precisionLabel;
+    private javax.swing.JTextField resolutionField;
+    private javax.swing.JLabel resolutionLabel;
+    private javax.swing.JTextField rocketWeightField;
     private javax.swing.JLabel rocketWeightLabel;
     // End of variables declaration//GEN-END:variables
 
