bf5eb18f7f9bfe12a3a2fe757c721aebaec163e4
Uli Koehler <ulikoehler@online.de>
2009-01-20 09:28:55 +0000

    BookCrawler: Added most code, will work soon

--- BookCrawler/src/BookCrawler.py
+++ BookCrawler/src/BookCrawler.py
@@ -10,37 +10,54 @@ __date__ ="$19.01.2009 19:52:53$"
 #####
 if __name__ == "__main__":
     #Ask for the URL
+    count = int(raw_input("Number of URLs to crawl: "))
     url = raw_input("Start URL: ")
     #Init the database
-    conn = sqlite3.connect(":memory") #//TODO change to a file and find a way to check if the table already exists
-    conn.execute('''CREATE TABLE Books(Name VARCHAR(100), ISBN VARCHAR(100), Price REAL, Pages INTEGER)''')
-    #Read the URL content
+    conn = sqlite3.connect(':memory') #//TODO change to a file and find a way to check if the table already exists
+    if not len(con.execute("SELECT COUNT(*) FROM sqlite_master WHERE name=\"Books\"").fetchone()) > 0:
+        conn.execute('''CREATE TABLE Books(Name VARCHAR(100), ISBN CHAR(14), Price REAL, Pages INTEGER)''')
+
+    urlQueue = []
+    htmlQueue = []
+
+    #Find the book price
+    titleRegex = re.compile("<span id=\"btAsinTitle\" >[:alnum:]+</span>", re.L)
+    isbnRegex = re.compile("<li><b>ISBN-13:</b> [1-9]{3,3}-[1-9]{10,10}</li>", re.L)
+    priceRegex = re.compile("<b class=\"priceLarge\">EUR [1-9,]+</b>", re.L)
+    pagesRegex = re.compile("<li><b>[:alpha:]+</b> \d+ Seiten</li>", re.L)
+    relatedRegex = re.compile("<td align=\"left\" valign=\"top\"><div style=\"width: 166px; height: 190px;\">\n<a href=\"[:alnum:/]+\"><div class=\"image-title\">", re.L)
+
+    #Main queue controller
+    while(len(queue > 0)):
+        parseUrl(queue.pop())
+
+def checkUrl(html, isbn)
+    global conn
+    #Check if this book has not yet been crawled
+    if len(conn.execute('''SELECT COUNT(Title) FROM BOOKS WHERE ISBN = ?''', isbn).fetchone()) > 0
+        return 1
+    return 0
+
+def parseUrl(url):
+    #Download the HTML page
     try:
         response = urllib2.urlopen(url)
         html = response.read()
     except Exception, detail:
         print "Error: ", detail
         exit(1)
-
-    print "Finished downloading"
-
-    urlQueue = []
-    htmlQueue = []
-
-    #Find the book price
-    titleRegex = re.compile("<span id=\"btAsinTitle\" >[:alnum:]+</span>")
-    isbnRegex = re.compile("<li><b>ISBN-13:</b> [1-9]{3,3}-[1-9]{10,10}</li>")
-    priceRegex = re.compile("<b class=\"priceLarge\">EUR [1-9,]+</b>")
-    
-def parseUrl():
     #Find the ISBN(-13)
     match = isbnRegex.search(html)
     matchStart = match.start() + len("<li><b>ISBN-13:</b> ")
     matchEnd = match.end() - len("</li>")
     isbn = html[matchStart:matchEnd]
     #Check if this book has not yet been crawled
-    c = conn.cursor()
-    
+    if checkUrl(html, isbn) > 0:
+        return 1
+    #Check if there are page-checks left
+    if counter == 0:
+        sys.exit(0)
+    counter -= 1
     #Find the title
     match = titleRegex.search(html)
     matchStart = match.start() + len("<span id=\"btAsinTitle\" >")
@@ -51,5 +68,19 @@ def parseUrl():
     matchStart = match.start() + len("<b class=\"priceLarge\">EUR ")
     matchEnd = match.end() - len("</b>")
     price = float(html[matchStart:matchEnd].replace(",","."))
+    #Find the page count
+    match = priceRegex.search(html)
+    matchStart = match.start() + len("<li><b>[:alpha:]+</b> ")
+    matchEnd = match.end() - len(" Seiten</li>")
+    pages = int(html[matchStart:matchEnd])
+    #Find related books
+    matches priceRegex.findall(html)
+    for match in matches:
+        matchStart = match.start() + len("<td align=\"left\" valign=\"top\"><div style=\"width: 166px; height: 190px;\">\n<a href=\"")
+        matchEnd = match.end() - len("\"><div class=\"image-title\">")
+        urlQueue.push(matchStart.matchEnd)
     #Insert the book into the database (c is initialized before)
-    c.execute('''INSERT INTO Books VALUES(''')
+    c = conn.cursor()
+    c.execute('''INSERT INTO Books VALUES(?,?,?,?)''', title, isbn, price, pages)
+
+
