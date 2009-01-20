df968aaa6c53c56a9af1fbc891c3026a1bef9ef1
Uli Koehler <ulikoehler@online.de>
2009-01-20 09:56:48 +0000

    BookCrawler: Code should work now (but untested yet)

--- BookCrawler/src/BookCrawler.py
+++ BookCrawler/src/BookCrawler.py
@@ -12,8 +12,9 @@ if __name__ == "__main__":
     #Ask for the URL
     count = int(raw_input("Number of URLs to crawl: "))
     url = raw_input("Start URL: ")
+    #Download the URL and push it into the queue
     #Init the database
-    conn = sqlite3.connect(':memory') #//TODO change to a file and find a way to check if the table already exists
+    conn = sqlite3.connect('books.sqlite3')
     if not len(con.execute("SELECT COUNT(*) FROM sqlite_master WHERE name=\"Books\"").fetchone()) > 0:
         conn.execute('''CREATE TABLE Books(Name VARCHAR(100), ISBN CHAR(14), Price REAL, Pages INTEGER)''')
 
@@ -31,6 +32,14 @@ if __name__ == "__main__":
     while(len(queue > 0)):
         parseUrl(queue.pop())
 
+def downloadUrl(url):
+    try:
+        response = urllib2.urlopen(url)
+        return response.read()
+    except Exception, detail:
+        print "Error: ", detail
+        exit(1)
+
 def checkUrl(html, isbn)
     global conn
     #Check if this book has not yet been crawled
@@ -38,49 +47,61 @@ def checkUrl(html, isbn)
         return 1
     return 0
 
-def parseUrl(url):
-    #Download the HTML page
-    try:
-        response = urllib2.urlopen(url)
-        html = response.read()
-    except Exception, detail:
-        print "Error: ", detail
-        exit(1)
-    #Find the ISBN(-13)
+def getISBN(html):
     match = isbnRegex.search(html)
     matchStart = match.start() + len("<li><b>ISBN-13:</b> ")
     matchEnd = match.end() - len("</li>")
-    isbn = html[matchStart:matchEnd]
+    return html[matchStart:matchEnd]
+
+def getTitle(html):
+    match = titleRegex.search(html)
+    matchStart = match.start() + len("<span id=\"btAsinTitle\" >")
+    matchEnd = match.end() - len("</span>")
+    return html[matchStart:matchEnd]
+
+def getPrice(html):
+    match = priceRegex.search(html)
+    matchStart = match.start() + len("<b class=\"priceLarge\">EUR ")
+    matchEnd = match.end() - len("</b>")
+    return float(html[matchStart:matchEnd].replace(",","."))
+
+def getPageCount(html):
+    match = priceRegex.search(html)
+    matchStart = match.start() + len("<li><b>[:alpha:]+</b> ")
+    matchEnd = match.end() - len(" Seiten</li>")
+    return int(html[matchStart:matchEnd])
+
+def parseHTML(html):
+    #Find the ISBN(-13)
+    isbn = getISBN(html)
     #Check if this book has not yet been crawled
     if checkUrl(html, isbn) > 0:
         return 1
+    ##################
+    ##Parse the data##
+    ##################
     #Check if there are page-checks left
     if counter == 0:
         sys.exit(0)
     counter -= 1
     #Find the title
-    match = titleRegex.search(html)
-    matchStart = match.start() + len("<span id=\"btAsinTitle\" >")
-    matchEnd = match.end() - len("</span>")
-    title = html[matchStart:matchEnd]
+    title = getTitle(html)
     #Find the price
-    match = priceRegex.search(html)
-    matchStart = match.start() + len("<b class=\"priceLarge\">EUR ")
-    matchEnd = match.end() - len("</b>")
-    price = float(html[matchStart:matchEnd].replace(",","."))
+    price = getPrice(html)
     #Find the page count
-    match = priceRegex.search(html)
-    matchStart = match.start() + len("<li><b>[:alpha:]+</b> ")
-    matchEnd = match.end() - len(" Seiten</li>")
-    pages = int(html[matchStart:matchEnd])
-    #Find related books
-    matches priceRegex.findall(html)
-    for match in matches:
-        matchStart = match.start() + len("<td align=\"left\" valign=\"top\"><div style=\"width: 166px; height: 190px;\">\n<a href=\"")
-        matchEnd = match.end() - len("\"><div class=\"image-title\">")
-        urlQueue.push(matchStart.matchEnd)
+    pages = getPageCount(html)
     #Insert the book into the database (c is initialized before)
     c = conn.cursor()
     c.execute('''INSERT INTO Books VALUES(?,?,?,?)''', title, isbn, price, pages)
+    #Find related books and download the matches and push them into the HTML queue
+    matches =  priceRegex.findall(html)
+    for match in matches:
+        matchStart = match.start() + len("<td align=\"left\" valign=\"top\"><div style=\"width: 166px; height: 190px;\">\n<a href=\"")
+        matchEnd = match.end() - len("\"><div class=\"image-title\">")
+        #Download the page and check if the book has already been covered, if not push it into queue
+        html = downloadUrl(html[matchStart:matchEnd])
+        if not checkUrl(html, getISBN(html)) > 0:
+            htmlQueue.push(html)
+    
 
 
