03d44ab960eb35de7f51aab2cfd2a4e09cf98d20
Uli Koehler <ulikoehler@online.de>
2009-01-20 10:01:39 +0000

    BookCrawler: Fixed some bugs (again)

--- BookCrawler/src/BookCrawler.py
+++ BookCrawler/src/BookCrawler.py
@@ -5,37 +5,6 @@ import sqlite3
 __author__="uli"
 __date__ ="$19.01.2009 19:52:53$"
 
-#####
-#Main
-#####
-if __name__ == "__main__":
-    htmlQueue = []
-    #Ask for the URL
-    counter = int(raw_input("Number of URLs to crawl: "))
-    url = raw_input("Start URL: ")
-    #Download the URL and push it into the queue
-    htmlQueue.push()
-    #Init the database
-    conn = sqlite3.connect('books.sqlite3')
-    if not len(con.execute("SELECT COUNT(*) FROM sqlite_master WHERE name=\"Books\"").fetchone()) > 0:
-        conn.execute('''CREATE TABLE Books(Name VARCHAR(100), ISBN CHAR(14), Price REAL, Pages INTEGER)''')
-
-    #Find the book price
-    titleRegex = re.compile("<span id=\"btAsinTitle\" >[:alnum:]+</span>", re.L)
-    isbnRegex = re.compile("<li><b>ISBN-13:</b> [1-9]{3,3}-[1-9]{10,10}</li>", re.L)
-    priceRegex = re.compile("<b class=\"priceLarge\">EUR [1-9,]+</b>", re.L)
-    pagesRegex = re.compile("<li><b>[:alpha:]+</b> \d+ Seiten</li>", re.L)
-    relatedRegex = re.compile("<td align=\"left\" valign=\"top\"><div style=\"width: 166px; height: 190px;\">\n<a href=\"[:alnum:/]+\"><div class=\"image-title\">", re.L)
-
-    #Main queue controller
-    #Start the HTML analyzer if there are page-checks left
-    while(len(queue > 0)):
-        if counter > 0:
-            parseUrl(queue.pop())
-            counter -= 1
-        else:
-            sys.exit(0)
-
 def downloadUrl(url):
     try:
         response = urllib2.urlopen(url)
@@ -44,10 +13,10 @@ def downloadUrl(url):
         print "Error: ", detail
         exit(1)
 
-def checkUrl(html, isbn)
+def checkUrl(html, isbn):
     global conn
     #Check if this book has not yet been crawled
-    if len(conn.execute('''SELECT COUNT(Title) FROM BOOKS WHERE ISBN = ?''', isbn).fetchone()) > 0
+    if len(conn.execute('''SELECT COUNT(Title) FROM BOOKS WHERE ISBN = ?''', isbn).fetchone()) > 0:
         return 1
     return 0
 
@@ -102,7 +71,38 @@ def parseHTML(html):
         #Download the page and check if the book has already been covered, if not push it into queue
         html = downloadUrl(html[matchStart:matchEnd])
         if not checkUrl(html, getISBN(html)) > 0:
-            htmlQueue.push(html)
+            htmlQueue.append(html)
+
+#####
+#Main
+#####
+if __name__ == "__main__":
+    htmlQueue = []
+    #Ask for the URL
+    counter = int(raw_input("Number of URLs to crawl: "))
+    url = raw_input("Start URL: ")
+    #Download the URL and push it into the queue
+    htmlQueue.append(downloadUrl(url))
+    #Init the database
+    conn = sqlite3.connect('books.sqlite3')
+    if not len(con.execute("SELECT COUNT(*) FROM sqlite_master WHERE name=\"Books\"").fetchone()) > 0:
+        conn.execute('''CREATE TABLE Books(Name VARCHAR(100), ISBN CHAR(14), Price REAL, Pages INTEGER)''')
+
+    #Find the book price
+    titleRegex = re.compile("<span id=\"btAsinTitle\" >[:alnum:]+</span>", re.L)
+    isbnRegex = re.compile("<li><b>ISBN-13:</b> [1-9]{3,3}-[1-9]{10,10}</li>", re.L)
+    priceRegex = re.compile("<b class=\"priceLarge\">EUR [1-9,]+</b>", re.L)
+    pagesRegex = re.compile("<li><b>[:alpha:]+</b> \d+ Seiten</li>", re.L)
+    relatedRegex = re.compile("<td align=\"left\" valign=\"top\"><div style=\"width: 166px; height: 190px;\">\n<a href=\"[:alnum:/]+\"><div class=\"image-title\">", re.L)
+
+    #Main queue controller
+    #Start the HTML analyzer if there are page-checks left
+    while(len(queue > 0)):
+        if counter > 0:
+            parseUrl(htmlQueue.pop())
+            counter -= 1
+        else:
+            sys.exit(0)
     
 
 
