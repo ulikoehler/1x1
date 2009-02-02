4796e58c6cde08b5cae7a6edc328091248b9c0b9
Uli Koehler <ulikoehler@online.de>
2009-02-02 16:21:59 +0000

    JOpenSSLGUI: Added panel to the key generating tabbed pane to extract the public key from a private key file

--- JOpenSSLGUI/src/jopensslgui/GenerateKeysTabbedPane.java
+++ JOpenSSLGUI/src/jopensslgui/GenerateKeysTabbedPane.java
@@ -38,15 +38,18 @@ public class GenerateKeysTabbedPane extends JTabbedPane {
         generateDSAKeysPanel = new jopensslgui.GenerateDSAKeysPanel();
         generateDHKeysPanel = new jopensslgui.GenerateDHKeysPanel();
         generateECKeysPanel = new jopensslgui.GenerateECKeysPanel();
+        extractPublicKeyPanel = new jopensslgui.ExtractPublicKeyPanel();
 
         addTab( i18n.getString("GenerateKeysTabbedPane.generateRSAKeysPanel.TabConstraints.tabTitle"), generateRSAKeysPanel); // NOI18N
         addTab( i18n.getString("GenerateKeysTabbedPane.generateDSAKeysPanel.TabConstraints.tabTitle"), generateDSAKeysPanel); // NOI18N
         addTab( i18n.getString("GenerateKeysTabbedPane.generateDHKeysPanel.TabConstraints.tabTitle"), generateDHKeysPanel); // NOI18N
         addTab( i18n.getString("GenerateKeysTabbedPane.generateECKeysPanel.TabConstraints.tabTitle"), generateECKeysPanel); // NOI18N
+        addTab( i18n.getString("GenerateKeysTabbedPane.extractPublicKeyPanel.TabConstraints.tabTitle"), extractPublicKeyPanel); // NOI18N
     }// </editor-fold>//GEN-END:initComponents
 
     private ResourceBundle i18n = ResourceBundle.getBundle("jopensslgui/Bundle"); //NOI18N
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private jopensslgui.ExtractPublicKeyPanel extractPublicKeyPanel;
     private jopensslgui.GenerateDHKeysPanel generateDHKeysPanel;
     private jopensslgui.GenerateDSAKeysPanel generateDSAKeysPanel;
     private jopensslgui.GenerateECKeysPanel generateECKeysPanel;
