d1e8a8b8fd55a5d0c86b347b73a9def2891f4c67
Uli Köhler <ulrich.koehler@helmholtz-muenchen.de>
2010-07-27 17:36:07 +0200

    Added old changed and .dep.inc .gitignore entry

---
+++ JCrypter/src/jcrypter/wrapper/CipherWrapper.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jcrypter.wrapper;
+
+/**
+ * An interface providing (basically) the same interface as Cipher
+ * but wrapping underlying classes
+ * @author uli
+ */
+public interface CipherWrapper
+{
+    
+}
